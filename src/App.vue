dfe7c30cfc5f61edf15b5f723a4fb10acded0be1
kevin temin <temin.kevin@gmail.com>
2020-10-17 18:53:31 +0200

    first version (no working)

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <template>
     <div id="app">
-        <Home/>
     </div>
 </template>
 
 <script>
     import Home from './components/Home.vue';
+
 
     export default {
         name: 'app',
--- src/main.js
+++ src/main.js
@@ -5,11 +5,32 @@ import VueMaterial from "vue-material";
 import 'vue-material/dist/vue-material.min.css';
 import 'vue-material/dist/theme/default.css';
 
+import VueRouter from 'vue-router';
+import Home from './components/Home.vue';
+
+
 Vue.use(VueMaterial);
+Vue.use(VueRouter);
 
 Vue.config.productionTip = true;
 
 
+const NotFound = { template: '<p>Page not found</p>' }
+
+const router = new VueRouter({
+    routes: [
+        {
+            path: '/home',
+            name: 'homepage',
+            component: Home
+        }
+    ]
+})
+
+router.redirect({
+    '/': '/home'
+})
 new Vue({
-    render: h => h(App)
+    component: App,
+    router: router
 }).$mount('#app');
